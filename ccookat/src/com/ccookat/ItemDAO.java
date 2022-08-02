4a3d2ab7f45d2a20f1a12c79f025b18b5519fbba
sally <105549970+SSaallyy@users.noreply.github.com>
2022-08-02 19:38:50 +0900

    공지사항 dao완성

--- ccookat/src/com/ccookat/ItemDAO.java
+++ ccookat/src/com/ccookat/ItemDAO.java
@@ -1,5 +1,20 @@
 package com.ccookat;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class ItemDAO {
+	
+	private Connection conn;
+	
+	public ItemDAO(Connection conn) {
+		
+		this.conn = conn;
+	}
+	
+	
+	
+	
+	
 
 }
