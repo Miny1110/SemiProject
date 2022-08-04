8c94e2b1bde8b9c9fc8414dd439c1c58c1e4bf01
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-08-04 11:27:50 +0900

    djkdjdjk

--- ccookat/src/com/svt/MainServlet.java
+++ ccookat/src/com/svt/MainServlet.java
@@ -1,5 +1,46 @@
 package com.svt;
 
-public class MainServlet {
+import java.io.IOException;
+import java.sql.Connection;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ccookat.ItemDAO;
+import com.util.DBConn;
+
+public class MainServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		doPost(req, resp);
+		
+	}
+	
+	protected void forward(HttpServletRequest req, HttpServletResponse resp, String url) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher(url);
+		
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+
+		Connection conn = DBConn.getconnection();
+		ItemDAO idao = new ItemDAO(conn);
+		
+		String cp = req.getContextPath();
+		String uri = req.getRequestURI();
+		String url;
+		
+	}
+	
 }
