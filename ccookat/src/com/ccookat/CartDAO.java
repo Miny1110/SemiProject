ebb9d886d05c9c50d3e6ed2f823198d843773a60
sally <105549970+SSaallyy@users.noreply.github.com>
2022-08-05 12:12:28 +0900

    장바구니시작하고 메인에서 경로끌어옴

--- ccookat/src/com/ccookat/CartDAO.java
+++ ccookat/src/com/ccookat/CartDAO.java
@@ -1,5 +1,15 @@
 package com.ccookat;
 
+import java.sql.Connection;
+
 public class CartDAO {
 
+	private Connection conn;
+	
+	public CartDAO(Connection conn) {
+		
+		this.conn = conn;
+		
+	}
+	
 }
