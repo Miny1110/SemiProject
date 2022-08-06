2612ea188e9cf3a30570d58e002e30f08008fdb0
sally <105549970+SSaallyy@users.noreply.github.com>
2022-08-06 15:15:40 +0900

    오류나는게정상~ 카트에

--- ccookat/src/com/svt/CartServlet.java
+++ ccookat/src/com/svt/CartServlet.java
@@ -15,6 +15,9 @@ import javax.servlet.http.HttpSession;
 import com.ccookat.CartDAO;
 import com.ccookat.CartDTO;
 import com.ccookat.CustomerInfo;
+import com.ccookat.NoticeDTO;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.util.DBConn;
 
 public class CartServlet extends HttpServlet {
@@ -53,7 +56,6 @@ public class CartServlet extends HttpServlet {
 			
 			String customerId = customerInfo.getCustomerId();
 				
-			System.out.println(customerId);
 			List<CartDTO> lists = ctdao.selectAll(customerId);
 			
 			if(lists==null) {				
@@ -68,7 +70,33 @@ public class CartServlet extends HttpServlet {
 					
 			url = "/cart/cartMain.jsp";
 			forward(request, response, url);	
-		}
+			
+		}else if(uri.indexOf("cartin.do")!=-1) {
+
+			//아이템 넘버랑, 데이터 카운트랑, 세션에 올라가 있는 아이디 받아서 
+			//카트에 인서트 시켜야함
+			HttpSession session = request.getSession();
+
+			CustomerInfo customerInfo = new CustomerInfo();
+
+			customerInfo = (CustomerInfo)session.getAttribute("customerInfo");
+
+			String customerId = customerInfo.getCustomerId();
+
+			CartDTO ctdto = new CartDTO();
+
+			int maxnum = ctdao.getMaxNum();
+
+			ctdto.setCartNum(maxnum+1);
+			ctdto.setCustomerId(customerId);
+			ctdto.setItemNum(Integer.parseInt(request.getParameter("itemNum")));
+
+			ctdao.insertData(ctdto);
+
+
+			url = cp + "/main/notice/list.do?noticeSearchKey="+mr.getParameter("noticeSearchKey");
+			response.sendRedirect(url);
+
 	}
 
 }
