74fa715b2585a1cef17f87d46c2fbbf26aa3d8c6
sally <105549970+SSaallyy@users.noreply.github.com>
2022-08-09 15:12:59 +0900

    Update CustomerOrderServlet.java

--- ccookat/src/com/svt/CustomerOrderServlet.java
+++ ccookat/src/com/svt/CustomerOrderServlet.java
@@ -101,11 +101,31 @@ public class CustomerOrderServlet extends HttpServlet {
 			customerInfo = (CustomerInfo)session.getAttribute("customerInfo");
 
 			if(customerInfo!=null) {
-
+				
+				String customerId = customerInfo.getCustomerId();	
+				
+				//주문정보 받아와야함
+				//detail테이블에 정보추가
+				CustomerOrderDTO ordto = new CustomerOrderDTO();
+				
+				ordto.setOrderNum(ordao.getMaxNum()+1);
+				ordto.setCustomerId(customerId);	
+				ordto.setCustomerOrderName(request.getParameter("customerOrderName"));
+				ordto.setCustomerOrderZipCode(request.getParameter("customerOrderZipCode"));			
+				ordto.setCustomerOrderTel(request.getParameter("customerOrderTel"));
+				ordto.setCustomerOrderAddr1(request.getParameter("customerOrderAddr1"));
+				ordto.setCustomerOrderAddr2(request.getParameter("customerOrderAddr2"));
+				ordto.setCustomerMemo(request.getParameter("customerMemo"));
+				ordto.setCustomerOrderPay(request.getParameter("customerOrderPay"));
+				ordto.setCustomerPrice(Integer.parseInt(request.getParameter("customerPrice")));
+				
+				ordao.insertOrderData(ordto);
+				
+				//detail페이지에 주문된상품 리스트 저장하고 
+				//장바구니에서 삭제하는 코드
+				
 				OrderDetailDTO oddto = null;
 				CustomerDTO cdto = new CustomerDTO();
-
-				String customerId = customerInfo.getCustomerId();
 
 				//장바구니에 있는 정보를 받아서
 				List<CartDTO> lists = ctdao.selectAll(customerId);
@@ -116,22 +136,27 @@ public class CustomerOrderServlet extends HttpServlet {
 				
 				
 				while(it.hasNext()) {
-				
+						
+					CartDTO cartdao = it.next();
+					
 					oddto = new OrderDetailDTO();
 					
+					oddto.setOrderNum(ordao.getMaxNum());
+					oddto.setItemNum(cartdao.getItemNum());
+					oddto.setItemCount(cartdao.getCartItemCount());
+					oddto.setCustomerId(customerId);
+					oddto.setCartTotPrice(cartdao.getCartTotPrice());
 					
+					oddao.insertDetailData(oddto);					
 				}
 				//cartdelete 해야함 어차피 장바구니 전체상품 다 지워야하니까
 				//id조회해서 그 id가 가지고있는거 다지우는걸로 하면될듯				
 				ctdao.orderdelete(customerId);
-				
-				//주문정보 받아와야함
-				//detail테이블에 정보추가
-				CustomerOrderDTO ordto = new CustomerOrderDTO();
-								
+						
 				//일단 메인으로감....결제완료페이지만들예정
-				url = "/main";
-				forward(request, response, url);	
+				url = "/main/custome/searchPwd_ok.do";
+				//forward(request, response, url);	
+				response.sendRedirect(url);
 				return;
 			}			
 			//로그인 안되어있을때 로그인창보내기
